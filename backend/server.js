6337b5e718254b97ac0f0528f4e6975183127da7
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-13 12:33:55 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import mongoose from 'mongoose';
 
 const app = express();
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
-    console.log(`server is running on http://localhost:${port}`)
-})
+
+const connectDB = async () => {
+    try {
+
+        const DB = await mongoose.connect(`${process.env.MONGODB_URL}`)
+        console.log('DB connected successfully')
+
+        app.listen(port, () => {
+            console.log(`server is running on http://localhost:${port}`)
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+connectDB();
+
