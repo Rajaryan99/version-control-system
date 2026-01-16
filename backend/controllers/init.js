248832f81ad49e2ee276fb86c3d9bfce6da1d09c
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-16 20:33:54 +0530

    implement repository initialization with config and commits directory

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -13,9 +13,18 @@ export async function intiRepo() {
     const commitPath = path.join(repoPath, 'commits');
 
     try {
+
+        await fs.mkdir(repoPath, { recursive: true });
+        await fs.mkdir(commitPath, { recursive: true });
+        await fs.writeFile(
+            path.join(repoPath, 'config.json'),
+            JSON.stringify({ bucket: 's3 bucket' })
+        );
+
+        console.log('Repository initalized!')
         
     } catch (error) {
-        
+        console.log('Error initialising repository', error)
     }
 }
 
