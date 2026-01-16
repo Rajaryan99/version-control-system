22244848ac2a65e536741bbf58b1a519c8198c0b
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-16 20:05:41 +0530

    initializating inti

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,22 @@
-export function intiRepo() {
-    console.log('Init command called')
+import FS from 'fs'
+import path from 'path'
+
+const fs = FS.promises;
+
+
+
+
+
+export async function intiRepo() {
+
+    const repoPath = path.resolve(process.cwd(), '.gitsh')
+    const commitPath = path.join(repoPath, 'commits');
+
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 // export default { intiRepo }
