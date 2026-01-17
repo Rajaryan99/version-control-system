452f8f0a151ff612e55bd51279e57869c2612c92
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-17 17:11:17 +0530

    using uuid version 4 for making a uique ID for our commits

--- backend/controllers/commit.js
+++ backend/controllers/commit.js
@@ -1,3 +1,10 @@
-export function commit() {
-    console.log('file is ready to push to repo')
+import FS from 'fs'
+import path from 'path'
+import { v4 as uuidv4 } from 'uuid';
+
+const fs = FS.promises;
+
+export async function commit(message) {
+
+
 }
