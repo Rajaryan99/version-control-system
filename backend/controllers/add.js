cb9de5c155ac8a69733201927366cf8cc76a9e89
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-17 16:43:35 +0530

    importing file system from node modules

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,3 +1,23 @@
-export function addRepo() {
-    console.log('File added  to the staging area')
+import FS from 'fs'
+import path from 'path'
+
+const fs = FS.promises
+
+
+
+
+export async function addRepo(filePath) {
+    const repoPath = path.resolve(process.cwd(), '.gitsh')
+    const stagingPath = path.join(repoPath, 'staging');
+
+    try {
+
+        await fs.mkdir(stagingPath, { recursive: true })
+        const fileName = path.basename(filePath);
+
+        
+        
+    } catch (error) {
+        console.log('Error in adding file : ', error)
+    }
 }
