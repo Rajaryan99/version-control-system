e0b93621f355a3e4b9da4985382d0f4dcf8664bf
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-18 16:04:39 +0530

    making a commit ID uisng uuidv4

--- backend/controllers/commit.js
+++ backend/controllers/commit.js
@@ -6,5 +6,17 @@ const fs = FS.promises;
 
 export async function commit(message) {
 
+    const repoPath = path.resolve(process.cwd(), '.gitsh')
+    const stagePath = path.join(repoPath, 'staging')
+    const commitPath = path.join(repoPath, 'commits')
 
+    try {
+
+        const commitID = uuidv4();
+        
+    } catch (error) {
+
+        console.log('Error in commiting file : ', error)
+
+    }
 }
