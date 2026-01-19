b3d0ad4d26a980b38e3d89ae37980636494e8d49
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-19 13:00:17 +0530

    added a pull command to pull commits from S3 bucket

--- backend/controllers/pull.js
+++ backend/controllers/pull.js
@@ -1,3 +1,47 @@
-export function pullRepo() {
-    console.log('file pulled from S3 buket')
+import FS from 'fs'
+import path from 'path'
+import {s3, S3_BUCKET}  from '../config/aws.config.js'
+
+const fs = FS.promises
+
+
+export async function pullRepo() {
+    const repoPath = path.resolve(process.cwd(), '.gitsh')
+    const commitsPath = path.join(repoPath, 'commits')
+    
+    try {
+
+        const data = await s3.listObjectsV2({
+            Bucket: S3_BUCKET,
+            Prefix: 'commits/',
+        }).promise();
+
+        const objects = data.Contents;
+
+        for (const object of objects) {
+            const key = object.Key;
+            const commitDir = path.join(
+                commitsPath,
+                path.dirname(key).split('/').pop()
+            )
+
+            await fs.mkdir(commitDir, { recursive: true });
+
+            const params = {
+                Bucket: S3_BUCKET,
+                Key: key
+            }
+
+            const fileContent = await s3.getObject(params).promise();
+            await fs.writeFile(path.join(repoPath, key), fileContent.Body)
+
+
+            console.log('All contents pulled from S3')
+
+        }
+
+        
+    } catch (error) {
+        console.error('unable to pull : ', error)
+    }
 }
