f6eaffe6bc2210d3c5dad2d1e061e008b1b0b212
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-21 12:49:50 +0530

    added a repostory schema

---
+++ backend/models/repoModel.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+const RepositorySchema = new Schema({
+
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    description: {
+        type: String,
+    },
+
+    content: [
+        {
+            type: String,
+        },
+    ],
+    visibility: {
+        type: Boolean,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    issues: [
+        {
+            type: Schema.Types.ObjectId,
+            ref:'Issue'
+        }
+    ]
+
+})
+
+export const Repository = mongoose.model('Repository', RepositorySchema)
+
