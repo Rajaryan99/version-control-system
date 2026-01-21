65558b00f898b05acd19b8015cab92910f57355b
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-21 13:35:29 +0530

    added a issue scham for all the issue in repo

---
+++ backend/models/issueModel.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const IssueSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['open', 'closed'],
+        default: 'open'
+    },
+    repository: {
+        type: Schema.Types.ObjectId,
+        ref: "Repository",
+        required: true
+    }
+})
+
+export const Issue = mongoose.model('Issue', IssueSchema);
