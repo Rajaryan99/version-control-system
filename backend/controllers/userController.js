91d19f0317218e44dfc6320604347bfacf41e6df
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-21 19:18:24 +0530

    added a user controller functinality

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+const getAllUsers = (req, res) => {
+    res.res('All user fetched')
+}
+
+const signup = (req, res) => {
+    res.send('Signing in')
+}
+
+const login = (req, res) => {
+    res.send('Login in')
+}
+
+const getUserProlfile = (req, res) => {
+    res.send('Profile Fetched')
+}
+
+const updateUserProfile = (req, res) => {
+    res.send('Profile Updated')
+}
+
+const deleteUserProfile = (req, res) => {
+    res.send('Profile deleted')
+}
