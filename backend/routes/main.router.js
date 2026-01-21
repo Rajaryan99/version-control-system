23da123bf983bf75d865d87c2ba5bc8191e922c0
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-21 20:02:18 +0530

    routing the user route to main route "

--- backend/routes/main.router.js
+++ backend/routes/main.router.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import { userRouter } from './user.router.js';
 
-
- export const mainRouter = express.Router();
+export const mainRouter = express.Router();
+ 
+mainRouter.use(userRouter)
 
 mainRouter.get('/', (req, res) => {
     res.send('hellow World')
--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -5,7 +5,8 @@ import  userController  from '../controllers/userController.js'
 export const userRouter = express.Router();
 
 userRouter.get('/allUsers', userController.getAllUsers)
-userRouter.get('/allUsers', userController.getAllUsers)
-userRouter.get('/allUsers', userController.getAllUsers)
-userRouter.get('/allUsers', userController.getAllUsers)
-userRouter.get('/allUsers', userController.getAllUsers)
+userRouter.post('/signup', userController.signup)
+userRouter.post('/login', userController.login)
+userRouter.get('/userProfile', userController.getUserProlfile)
+userRouter.put('/updateProfile', userController.updateUserProfile)
+userRouter.delete('/deleteProfile', userController.deleteUserProfile)
