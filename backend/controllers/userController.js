8c37b451f8876097930751faf896ed0cb7031813
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-22 19:55:17 +0530

    connected a mongodb to create a mongo clinet for smooth workflow

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,22 @@
-  const getAllUsers = (req, res) => {
-    res.res('All user fetched')
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+import { MongoClient } from 'mongodb'
+import 'dotenv/config'
+
+
+const uri = process.env.MONGODB_URL
+
+let client;
+
+async function connectClient() {
+    if (!client) {
+        client = new MongoClient(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        await client.connect()
+    }
 }
 
  const signup = (req, res) => {
@@ -9,6 +26,11 @@
   const login = (req, res) => {
     res.send('Login in')
 }
+
+const getAllUsers = (req, res) => {
+    res.res('All user fetched')
+}
+
 
  const getUserProlfile = (req, res) => {
     res.send('Profile Fetched')
