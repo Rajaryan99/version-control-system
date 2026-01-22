1052b5617e41f3a3267ce2cb8c857d89db031c83
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-22 16:01:12 +0530

    updated issue update roter

--- backend/routes/issue.router.js
+++ backend/routes/issue.router.js
@@ -5,7 +5,7 @@ import issueController from '../controllers/issueController.js';
 export const issueRouter = express.Router();
 
 issueRouter.post('/issue/create', issueController.createIssue)
-issueRouter.put('/issue/:id', issueController.updateIssueById)
-issueRouter.delete('/issue/:id', issueController.deleteIssueById)
+issueRouter.put('/issue/update/:id', issueController.updateIssueById)
+issueRouter.delete('/issue/delete/:id', issueController.deleteIssueById)
 issueRouter.get('/issue/all', issueController.getAllIssue)
 issueRouter.get('/issue/:id', issueController.getIssueById)
