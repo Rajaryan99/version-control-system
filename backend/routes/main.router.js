f0661304ff3c0e7672641e0cda7bd3ed3fbebae8
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-22 13:24:53 +0530

    all the issue routes added to main routes in backend and tested

--- backend/routes/main.router.js
+++ backend/routes/main.router.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { userRouter } from './user.router.js';
 import { repoRouter } from './repo.router.js';
+import { issueRouter } from './issue.router.js';
 
 export const mainRouter = express.Router();
  
 mainRouter.use(userRouter)
 mainRouter.use(repoRouter)
+mainRouter.use(issueRouter)
 
 mainRouter.get('/', (req, res) => {
     res.send('hellow World')
