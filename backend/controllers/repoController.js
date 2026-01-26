1712216a6299a3145dd002b2a8e0f749398055ad
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-26 21:40:59 +0530

    destructuring the objects from model

--- backend/controllers/repoController.js
+++ backend/controllers/repoController.js
@@ -1,33 +1,50 @@
-const createRepository = (req, res) => {
-    res.send('Repository created!')
+import mongoose from 'mongoose'
+import Repository from '../models/repoModel.js'
+import issue from '../models/issueModel.js'
+import user from '../models/userModel.js'
+
+
+const createRepository = async (req, res) => {
+    const { owner, name, issues, content, description, visibility } = req.body; 
+    
+    try {
+
+        if (!name) {
+            return res.status(404).json({Error: "Repository name is required!!"})
+        }
+        
+    } catch (error) {
+        console.error('Error during repository creation :', error.message)
+        res.status(500).send('Server error')
+    }
 }
 
-const getAllRepositories = (req, res) => {
+const getAllRepositories = async (req, res) => {
     res.send('All Repository fetched!')
 }
 
-const fetchRepositoryById = (req, res) => {
+const fetchRepositoryById = async (req, res) => {
     res.send(' Repository detailed fetched!')
 }
 
-const fetchRepositoryByName = (req, res) => {
+const fetchRepositoryByName = async (req, res) => {
     res.send(' Repository detailed by name fetched!')
 }
 
-const fetchRepositoryForCurrewntUser = (req, res) => {
+const fetchRepositoryForCurrewntUser = async (req, res) => {
     res.send(' Repository for logedin yser Fetched!')
 }
 
-const updateRepositoryById = (req, res) => {
+const updateRepositoryById = async (req, res) => {
     res.send(' Repository updated!')
 }
 
-const  toggleVisbilityById = (req, res) => {
+const  toggleVisbilityById = async (req, res) => {
     res.send(' Repository visibiity')
 }
 
 
-const deleteRepositoryById = (req, res) => {
+const deleteRepositoryById = async (req, res) => {
     res.send(' Repository deleted!')
 }
 
