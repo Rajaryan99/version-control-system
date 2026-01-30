1ecd5a4465ba96c232d8bc39fbecef3696b20953
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-30 12:50:10 +0530

    added a update issue functionlaity

--- backend/controllers/issueController.js
+++ backend/controllers/issueController.js
@@ -21,7 +21,7 @@ const createIssue = async (req, res) => {
         res.status(201).json({ message: "IssueCreated" })
     
 } catch (error) {
-    console.error('Error creating  issue   :', error.message)
+    console.error('Error during creating  issue   :', error.message)
     res.status(500).send('Server error')
 }
    
@@ -29,7 +29,30 @@ const createIssue = async (req, res) => {
 }
 
 const updateIssueById = async (req, res) => {
-    res.send('Issue update')
+
+    const { id } = req.params;
+    const { title, description, status } = req.body;
+
+    try {
+
+        const issue = await Issue.findById(id);
+        if (!issue) {
+            return res.status(404).json({message: "issue not Found"})
+        }
+
+        issue.title = title;
+        issue.description = description;
+        issue.status = status;
+
+       const updatedIssue =  await issue.save();
+
+        res.status(200).json({message: "Issue updated"}, updatedIssue)
+
+        
+    } catch (error) {
+        console.error('Error during updating  issue   :', error.message)
+        res.status(500).send('Server error')
+    }
 }
 
 const deleteIssueById = async (req, res) => {
