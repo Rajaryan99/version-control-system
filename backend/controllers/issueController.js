397572ee0894d7c9176a4ea590a9a4dd76eb5173
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-30 12:43:10 +0530

    issue created

--- backend/controllers/issueController.js
+++ backend/controllers/issueController.js
@@ -1,21 +1,46 @@
+import mongoose from 'mongoose'
+import { Repository } from '../models/repoModel.js'
+import { Issue } from '../models/issueModel.js'
+import { User } from '../models/userModel.js'
 
-const createIssue = (req, res) => {
-    res.send('Issue Created')
+
+
+const createIssue = async (req, res) => {
+    const { title, description } = req.body;
+    const { id } = req.params;
+    try {
+    
+        const issue = new Issue({
+            title,
+            description,
+            repository: id,
+        })
+
+        await issue.save();
+
+        res.status(201).json({ message: "IssueCreated" })
+    
+} catch (error) {
+    console.error('Error creating  issue   :', error.message)
+    res.status(500).send('Server error')
+}
+   
+
 }
 
-const updateIssueById = (req, res) => {
+const updateIssueById = async (req, res) => {
     res.send('Issue update')
 }
 
-const deleteIssueById = (req, res) => {
+const deleteIssueById = async (req, res) => {
     res.send('Issue deleted')
 }
 
-const getAllIssue = (req, res) => {
+const getAllIssue = async (req, res) => {
     res.send('all issue fetched')
 }
 
-const getIssueById = (req, res) => {
+const getIssueById = async (req, res) => {
     res.send('get issue details')
 }
 
