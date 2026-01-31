990f40f8672fe11c395ce62590fc18ab5f989efb
Rajaryan singh <raj.aryan2518@gmail.com>
2026-01-31 21:15:06 +0530

    added routes for the fronetend

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useNavigate, useRoutes } from 'react-router-dom';
+
+
+// pages list
+
+import Dashboard from './components/dashboard/Dashboard.jsx'
+import Profile from './components/user/Profile.jsx'
+import Signup from './components/auth/Signup.jsx';
+import Login from './components/auth/Login.jsx'
+
+
