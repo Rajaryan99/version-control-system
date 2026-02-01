cd2717e3e351abbb3462b7947c69b7f2b367eb10
Rajaryan singh <raj.aryan2518@gmail.com>
2026-02-01 13:26:53 +0530

    setup the Projects Routers flow to Routing to diffetent pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <BrowserRouter>
-        <App />
+        <ProjectRoutes />
       </BrowserRouter>
     </AuthProvider>
   </StrictMode>,
