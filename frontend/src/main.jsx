9cd9d3bb30a82726a937712737cf50090ac9493c
Rajaryan singh <raj.aryan2518@gmail.com>
2026-02-01 13:21:01 +0530

    setup the Browser Router to nevigate through various Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './authContext.jsx'
+import ProjectRoutes from './Routes.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>,
 )
