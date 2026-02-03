a7e9f289d39ef388ee6e5fb66f5395ca90fced4e
Rajaryan singh <raj.aryan2518@gmail.com>
2026-02-03 23:34:22 +0530

    adding styles in signup from

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -6,9 +6,18 @@ import logo from '../../assets/github-mark-white.svg'
 export default function Signup() {
   return (
     <div className='main'>
-      <div className="gitLogo">
-        <img src={logo} alt="" className='signupLogo' />
+      <div className="gitLogo"> 
       </div>
+      <div className="signupBox">
+        <label htmlFor="username">Username</label>
+        <input type="text" placeholder='Enter Username' id='username' />
+        <label htmlFor="email">Email</label>
+        <input type="text" placeholder='Enter email' id='email' />
+        <label htmlFor="password">Password</label>
+        <input type="password" placeholder='Enter password' id='password' />
+        <button>Signup</button>
+      </div>
+      <div className="loginBox"></div>
       
     </div>
   )
