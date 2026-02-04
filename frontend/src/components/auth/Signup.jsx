ac152c63ef3fffd3046180c4e95f5bb3434c647b
Rajaryan singh <raj.aryan2518@gmail.com>
2026-02-04 11:33:52 +0530

    handled the signup submit funcionality with localstorage for token

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import './auth.css'
 import logo from '../../assets/github-mark-white.svg'
+import {useAuth} from '../../authContext.jsx'
 
 export default function Signup() {
+
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      setLoading(true);
+      const res = await axios.post("http://localhost:3000/signup", {
+        email: email,
+        password: password,
+        username: username
+      });
+
+      localStorage.setItem('token', res.data.token);
+      localStorage.setItem('userId', res.data.userId);
+
+      setCurrentUser(res.data.userId);
+      setLoading(false);
+
+      window.location.herf = '/'
+      
+    } catch (error) {
+      console.error('Signup error', error.message)
+    }
+  }
+
   return (
     <div className='main'>
 
@@ -13,13 +45,22 @@ export default function Signup() {
       </div>
 
       <div className="signupBox">
+        <div>
         <label htmlFor="username">Username</label>
-        <input type="text" placeholder='Enter Username' id='username' />
+          <input type="text" placeholder='Enter Username' id='username' />
+        </div>
+
+        <div>
         <label htmlFor="email">Email</label>
         <input type="text" placeholder='Enter email' id='email' />
+        </div>
+
+        <div>
         <label htmlFor="password">Password</label>
         <input type="password" placeholder='Enter password' id='password' />
-        <button>Signup</button>
+        </div>
+
+        <button onClick={handleSignup}>Signup</button>
       </div>
 
       <div className="loginBox">
