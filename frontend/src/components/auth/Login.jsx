4ace627ba11ff517f797a824cf6c603b749328df
Rajaryan singh <raj.aryan2518@gmail.com>
2026-02-04 13:11:09 +0530

    functionality to login user

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './auth.css'
+import logo from '../../assets/github-mark-white.svg'
+import { useAuth } from '../../authContext.jsx'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
+
+  
+  useEffect(() => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('userId')
+    setCurrentUser(null)
+  })
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const { currentUser, setCurrentUser } = useAuth()
+
+
+  const handlelogin = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      setLoading(true);
+      const res = await axios.post("http://localhost:3000/login", {
+        email: email,
+        password: password,
+      });
+
+
+      localStorage.setItem('token', res.data.token);
+      localStorage.setItem('userId', res.data.userId);
+
+      setCurrentUser(res.data.userId);
+      setLoading(false);
+
+      window.location.herf = '/'
+
+
+    } catch (error) {
+      console.error('login error', error.message);
+      alert("Login Failed!")
+      setLoading(false)
+    }
+
+}
+
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='main'>
+
+      <div className="gitLogo">
+        <img src={logo} alt="" />
+        <h2>Log In</h2>
+      </div>
+
+      <div className="signupBox">
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Enter email'
+            id='email' />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Enter password'
+            id='password' />
+        </div>
+
+        <button
+          onClick={handlelogin} disabled={loading}>{loading ? "Loading..." : "Login"}</button>
+      </div>
+
+      <div className="loginBox">
+        <p>New to GitHub? <Link to="/signup">Create an account</Link></p>
+
+      </div>
     </div>
   )
 }
