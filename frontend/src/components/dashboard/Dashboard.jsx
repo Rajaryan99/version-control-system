f0729b0a22d7f95647ed0f0350a3d57c99603e74
Rajaryan singh <raj.aryan2518@gmail.com>
2026-02-05 12:57:29 +0530

    fetching repostories from backend database

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 export default function Dashboard() {
+
+
+  const [repositories, setRepositories] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestedRepositories, setSuggestedRepositories] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+
+    const fetchRepositories = async () => {
+      const res = await fetch(`http://localhost:3000/repo/user/${userId}`)
+
+      const data = await res.json();
+      console.log(data)
+    };
+
+    fetchRepositories();
+  }, [])
+
   return (
     <div>
-      <h1>Dashboeard</h1>
+      <h1></h1>
     </div>
   )
 }
