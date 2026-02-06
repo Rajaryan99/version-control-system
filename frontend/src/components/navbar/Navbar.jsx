f5ec99ebd752fb514e1fdf2b49c095ac02e359cb
Rajaryan singh <raj.aryan2518@gmail.com>
2026-02-06 23:54:22 +0530

    nevBar added

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
+import logo from '../../assets/github-mark-white.svg'
+
 
 export default function Navbar() {
-  return (
-      <nav>
-          <Link to='/'>
-          <div>
-              
-              <img src="https://github.com/images/modules/logos_page/GitHub-Mark.png" alt="Github Logo" />
-              <h2>Github</h2>
-          </div>
-          </Link>
-          <div>
-              <Link to='/create'>
-                  <p>Create a Repository</p>
-              </Link>
-              <Link to='/profile'>
-                  <p>Profile</p>
-              </Link>
+    return (
+        <nav>
+            <Link to='/'>
+                <div className='logo'>
 
-          </div>
-    </nav>
-  )
+                    <img src={logo} alt="Github Logo" />
+                    <h2>Github</h2>
+                </div>
+            </Link>
+            <div className='profile'>
+                <div className='AIicons'>
+                    <a href=""><i class="fa-brands fa-openai" style={{fontSize:"18px"}}></i> OpenAI</a>
+                </div>
+                <div className='create'>
+                    <Link to='/create'>
+                        <p><i class="fa-regular fa-folder" ></i> Create a Repository</p>
+                    </Link>
+                </div>
+                <div className='pro'>
+                    <Link to='/profile'>
+                        <p> <i class="fa-regular fa-user"></i> Profile</p>
+                    </Link>
+                </div>
+
+            </div>
+
+
+        </nav>
+    )
 }
