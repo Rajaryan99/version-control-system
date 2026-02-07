787257577174a47ecf46a2e4d5264ef58a5e76fd
Rajaryan singh <raj.aryan2518@gmail.com>
2026-02-07 12:58:36 +0530

    added a stared reposotry and overview components UI in profile page

--- frontend/src/components/user/Profile.jsx
+++ frontend/src/components/user/Profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar'
 import './profile.css'
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import StarIcon from '@mui/icons-material/Star';
 
 export default function Profile() {
   return (
@@ -8,7 +10,18 @@ export default function Profile() {
       <Navbar/>
     <div className='profilepage'>
 
-      <h1>Profile</h1>
+        <div className="header">
+          <div>
+            <MenuBookIcon />
+            <p>Overview</p>
+          </div>
+          <div>
+            <StarIcon/>
+            <p>Stared Repository</p>
+          </div>
+        </div>
+
+        
       </div>
     </>
   )
