b255a5f2d5616715542995b28b333c52eeb6ce31
Rajaryan singh <raj.aryan2518@gmail.com>
2026-02-07 14:04:03 +0530

    profile page is dynamic with functionality

--- frontend/src/components/user/Profile.jsx
+++ frontend/src/components/user/Profile.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../navbar/Navbar'
 import './profile.css'
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import StarIcon from '@mui/icons-material/Star';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Profile() {
+
+  const navigate = useNavigate();
+  const [userDetails, setUserDetails] = useState({
+    name: "Username"
+  })
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      const userId = localStorage.getItem("userId");
+
+      if (userId) {
+        try {
+
+          const res = await axios.get(`http://localhost:3000/userProfile/${userId}`)
+          console.log(res)
+          setUserDetails(res.data)
+          
+        } catch (error) {
+          console.error("Fetching user in frontend error", error)
+        }
+      }
+    }
+
+fetchUserDetails()
+  }, [])
+
+
+
   return (
     <>
      
@@ -24,9 +54,9 @@ export default function Profile() {
 
         <div className='profileDetails'>
           <div className='imgDiv'>
-            <img src="" alt="" />
+            <img src="\src\assets\profile.png" alt="" />
           </div>
-          <h3>UserName</h3>
+          <h3>{ userDetails.username}</h3>
 
           <button className='followbtn'>Follow</button>
 
