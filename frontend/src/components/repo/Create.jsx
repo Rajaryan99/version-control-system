e612becfab660ea580e57ea92044ea0c07dde1cd
Rajaryan singh <raj.aryan2518@gmail.com>
2026-02-09 11:55:40 +0530

    added somr styling to reposiotry name owner in new repo component

--- frontend/src/components/repo/Create.jsx
+++ frontend/src/components/repo/Create.jsx
@@ -17,35 +17,41 @@ export default function Create() {
                 <div className="create">
                     <h2>General</h2>
                     <div className="repoName">
-                        <p>Owner*</p>
-                        <p>UsetId</p>
-                        <div>
+                        <div className="owner">
+                            <p>Owner*</p>
+                            <p className='id'>userid</p>
+                        </div>
+                        
+                        <div className='name'>
                             <label htmlFor="repo">Repository name*</label>
                             <input type="text" name="repo" id="repo" />
                         </div>
-                        <p>Great repository names are short and memorable. How about ?</p>
 
-                        <div className="description">
-                            <label htmlFor="des">Description</label>
-                            <input type="text" name='des' id='des' />
-                            <p>0 / 350 character</p>
-                        </div>
-
-                        <div className="visibility">
-                            <h2>Visibility</h2>
-                            <div className="public">
-                                <input type="radio" id='Visibility' />
-                                <label htmlFor="visibility">Public</label>
-                                <p>Any one on the internet can see this repository. You choose who can commit.</p>
-                            </div>
-                            <div className="privat">
-                                <input type="radio" id='Visibility' />
-                                <label htmlFor="visibility">Private</label>
-                                <p>You choose who can see and commit to this repository.</p>
-                            </div>
-
-                        </div>
                     </div>
+                    <p className='desc'>Great repository names are short and memorable. How about ?</p>
+
+
+                    <div className="description">
+                        <label htmlFor="des">Description</label>
+                        <input type="text" name='des' id='des' />
+                        <p>0 / 350 character</p>
+                    </div>
+
+                    <div className="visibility">
+                        <h2>Visibility</h2>
+                        <div className="public">
+                            <input type="radio" id='Visibility' />
+                            <label htmlFor="visibility">Public</label>
+                            <p>Any one on the internet can see this repository. You choose who can commit.</p>
+                        </div>
+                        <div className="privat">
+                            <input type="radio" id='Visibility' />
+                            <label htmlFor="visibility">Private</label>
+                            <p>You choose who can see and commit to this repository.</p>
+                        </div>
+
+                    </div>
+
                 </div>
 
             </div>
